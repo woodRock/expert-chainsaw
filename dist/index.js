81eaf4a5c5cc26a948a3b88b71e0ea6d011e7476
woodRock <j.r.h.wood98@gmail.com>
2021-02-07 23:38:19 +1300

    Fetch from Open Weather API

--- dist/index.js
+++ dist/index.js
@@ -1664,11 +1664,21 @@ exports.FetchError = FetchError;
 
 const fetch = __nccwpck_require__(467);
 const fs = __nccwpck_require__(747);
+const core = __nccwpck_require__(396);
 
 const FILE_NAME = "./README.md";
 const ENCODING = "utf8";
 const TAG_OPEN = `<!--- FEED-START -->`;
 const TAG_CLOSE = `<!--- FEED-END --->`;
+
+async function fetchWeather() {
+  const API_KEY = core.getInput("OPEN_WEATHER_TOKEN");
+  const city = "Wellington,New Zealand";
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&mode=html`;
+  const weather = await fetch(URL);
+  const weatherText = await articles.text();
+  return weatherText;
+}
 
 async function run() {
   const readme = fs.readFileSync(FILE_NAME, ENCODING);
@@ -1676,7 +1686,7 @@ async function run() {
   const indexAfter = readme.indexOf(TAG_CLOSE);
   const before = readme.substring(0, indexBefore);
   const after = readme.substring(indexAfter);
-  const input = "Hello, World!";
+  const input = fetchWeather();
   const editedReadme = `
     ${before}
     ${input}
@@ -1692,6 +1702,14 @@ try {
 } catch (error) {
   console.log(error);
 }
+
+
+/***/ }),
+
+/***/ 396:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
 
 
 /***/ }),
--- src/action.js
+++ src/action.js
@@ -1,10 +1,20 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
+const core = require("@actions/core");
 
 const FILE_NAME = "./README.md";
 const ENCODING = "utf8";
 const TAG_OPEN = `<!--- FEED-START -->`;
 const TAG_CLOSE = `<!--- FEED-END --->`;
+
+async function fetchWeather() {
+  const API_KEY = core.getInput("OPEN_WEATHER_TOKEN");
+  const city = "Wellington,New Zealand";
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&mode=html`;
+  const weather = await fetch(URL);
+  const weatherText = await articles.text();
+  return weatherText;
+}
 
 async function run() {
   const readme = fs.readFileSync(FILE_NAME, ENCODING);
@@ -12,7 +22,7 @@ async function run() {
   const indexAfter = readme.indexOf(TAG_CLOSE);
   const before = readme.substring(0, indexBefore);
   const after = readme.substring(indexAfter);
-  const input = "Hello, World!";
+  const input = fetchWeather();
   const editedReadme = `
     ${before}
     ${input}
