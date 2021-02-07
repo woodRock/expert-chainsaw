4cead25b7be9f24d3c0d7cac249600ae12418e8d
woodRock <j.r.h.wood98@gmail.com>
2021-02-07 23:06:07 +1300

    Write to README automatically

--- src/action.js
+++ src/action.js
@@ -1,8 +1,27 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
 
+const FILE_NAME = "./README.md";
+const ENCODING = "UTF8";
+const TAG_OPEN = `<!--- FEED-START -->`;
+const TAG_CLOSE = `<!--- FEED-END --->`;
+
 async function run() {
-  console.log("Hello, World!");
+  const readme = fs.readFileSync(FILE_NAME, ENCODING);
+  const indexBefore = readme.indexOf(TAG_OPEN) + TAG_OPEN.length;
+  const indexAfter = readme.indexOf(TAG_CLOSE);
+  const before = readme.substring(0, indexBefore);
+  const after = readme.substring(indexAfter);
+  const input = "Hello, World!";
+  const editedReadme = `
+    ${before}
+    ${input}
+    ${after}`;
+  fs.writeFileSync(FILE_NAME, editedReadme.trim());
 }
 
-run();
+try {
+  run();
+} catch (error) {
+  console.log(error);
+}
